1cbe157bd3ba0f5bd88b85e801d7697b91fdfa50
Kasper Frank <kasperdaff@gmail.com>
2015-01-15 19:55:20 +0100

    Added new constructor to DataContextScopeFactory accepting a Func<DataContext> in addition to the one accepting an IDataContextFactory.

---
+++ DataContextScope/DataContextFactory.cs
@@ -0,0 +1,30 @@
+﻿/* 
+ * Copyright (C) 2015 Kasper Frank
+ * http://www.geturi.com
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+using System;
+using System.Data.Linq;
+
+namespace Geturi.Data.Linq
+{
+  class DataContextFactory : IDataContextFactory
+  {
+    private readonly Func<DataContext> _factory;
+
+    public DataContextFactory(Func<DataContext> factory)
+    {
+      if (factory == null)
+        throw new ArgumentNullException("factory");
+
+      _factory = factory;
+    }
+
+    public TDataContext CreateDataContext<TDataContext>() where TDataContext : DataContext
+    {
+      return (TDataContext)_factory();
+    }
+  }
+}
--- DataContextScope/DataContextScopeFactory.cs
+++ DataContextScope/DataContextScopeFactory.cs
@@ -7,56 +7,65 @@
  */
 using System;
 using System.Data;
+using System.Data.Linq;
 using Geturi.Data.Linq;
 
 namespace Geturi.Data.Linq
 {
-    public class DataContextScopeFactory : IDataContextScopeFactory
+  public class DataContextScopeFactory : IDataContextScopeFactory
+  {
+    private readonly IDataContextFactory _dataContextFactory;
+
+    public DataContextScopeFactory(IDataContextFactory dataContextFactory = null)
     {
-        private readonly IDataContextFactory _dataContextFactory;
-
-        public DataContextScopeFactory(IDataContextFactory dataContextFactory = null)
-        {
-            _dataContextFactory = dataContextFactory;
-        }
-
-        public IDataContextScope Create(DataContextScopeOption joiningOption = DataContextScopeOption.JoinExisting)
-        {
-            return new DataContextScope(
-                joiningOption: joiningOption, 
-                readOnly: false, 
-                isolationLevel: null, 
-                dataContextFactory: _dataContextFactory);
-        }
-
-        public IDataContextReadOnlyScope CreateReadOnly(DataContextScopeOption joiningOption = DataContextScopeOption.JoinExisting)
-        {
-            return new DataContextReadOnlyScope(
-                joiningOption: joiningOption, 
-                isolationLevel: null, 
-                dataContextFactory: _dataContextFactory);
-        }
-
-        public IDataContextScope CreateWithTransaction(IsolationLevel isolationLevel)
-        {
-            return new DataContextScope(
-                joiningOption: DataContextScopeOption.ForceCreateNew, 
-                readOnly: false, 
-                isolationLevel: isolationLevel, 
-                dataContextFactory: _dataContextFactory);
-        }
-
-        public IDataContextReadOnlyScope CreateReadOnlyWithTransaction(IsolationLevel isolationLevel)
-        {
-            return new DataContextReadOnlyScope(
-                joiningOption: DataContextScopeOption.ForceCreateNew, 
-                isolationLevel: isolationLevel, 
-                dataContextFactory: _dataContextFactory);
-        }
-
-        public IDisposable SuppressAmbientContext()
-        {
-            return new AmbientContextSuppressor();
-        }
+      _dataContextFactory = dataContextFactory;
     }
+
+    public DataContextScopeFactory(Func<DataContext> dataContextFactory)
+    {
+      if (dataContextFactory != null)
+      {
+        _dataContextFactory = new DataContextFactory(dataContextFactory);
+      }
+    }
+
+    public IDataContextScope Create(DataContextScopeOption joiningOption = DataContextScopeOption.JoinExisting)
+    {
+      return new DataContextScope(
+          joiningOption: joiningOption,
+          readOnly: false,
+          isolationLevel: null,
+          dataContextFactory: _dataContextFactory);
+    }
+
+    public IDataContextReadOnlyScope CreateReadOnly(DataContextScopeOption joiningOption = DataContextScopeOption.JoinExisting)
+    {
+      return new DataContextReadOnlyScope(
+          joiningOption: joiningOption,
+          isolationLevel: null,
+          dataContextFactory: _dataContextFactory);
+    }
+
+    public IDataContextScope CreateWithTransaction(IsolationLevel isolationLevel)
+    {
+      return new DataContextScope(
+          joiningOption: DataContextScopeOption.ForceCreateNew,
+          readOnly: false,
+          isolationLevel: isolationLevel,
+          dataContextFactory: _dataContextFactory);
+    }
+
+    public IDataContextReadOnlyScope CreateReadOnlyWithTransaction(IsolationLevel isolationLevel)
+    {
+      return new DataContextReadOnlyScope(
+          joiningOption: DataContextScopeOption.ForceCreateNew,
+          isolationLevel: isolationLevel,
+          dataContextFactory: _dataContextFactory);
+    }
+
+    public IDisposable SuppressAmbientContext()
+    {
+      return new AmbientContextSuppressor();
+    }
+  }
 }
